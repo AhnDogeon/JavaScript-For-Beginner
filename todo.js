5a60404576a72eca16e537ef931db00df324cb7d
AhnDogeon <qltiqlti@gmail.com>
2021-01-10 18:09:19 +0900

    20210110 | Making a To Do List 1

---
+++ todo.js
@@ -0,0 +1,40 @@
+const toDoform = document.querySelector(".js-toDoForm"),
+    toDoInput = toDoform.querySelector("input"),
+    toDoList = document.querySelector(".js-toDoList");
+
+const TODOS_LS = "toDos";
+
+function paintToDo(text){
+    console.log(text);
+    const li = document.createElement("li");
+    const delBtn = document.createElement("button");
+    delBtn.innerText = "❌";
+    const span = document.createElement("span");
+    span.innerText = text;
+    li.appendChild(delBtn);
+    li.appendChild(span);
+    toDoList.appendChild(li);
+}
+
+function handleSubmit(event){
+    event.preventDefault();
+    const currentValue = toDoInput.value;
+    paintToDo(currentValue);
+    toDoInput.value = "";
+}
+
+function loadToDos() {
+    const toDos = localStorage.getItem(TODOS_LS);
+    if (toDos !== null){
+        // form이 항상 Showing 이므로 else 부분은 딱히 안넣어줘도 될 듯
+
+    }
+
+}
+
+function init(){
+    loadToDos();
+    toDoform.addEventListener("submit", handleSubmit);
+}
+
+init();
