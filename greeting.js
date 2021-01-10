a52b66941a9b492f9382ad5dfb67f9936f618f3a
AhnDogeon <qltiqlti@gmail.com>
2021-01-10 15:35:45 +0900

    20210110| Saving the User Name 2

--- greeting.js
+++ greeting.js
@@ -5,6 +5,23 @@ const form = document.querySelector(".js-form"),
 const USER_LS = "currentUser",
     SHOWING_CN = "showing";
 
+function saveName(text){
+    localStorage.setItem(USER_LS, text);
+}
+
+function handleSubmit(event){
+    event.preventDefault(); // event submit 동작에 의한 새로고침 방지 
+    const currentValue = input.value;
+    console.log(currentValue);
+    paintGreeting(currentValue);
+    saveName(currentValue);
+}
+
+function askForName(){
+    form.classList.add(SHOWING_CN);
+    form.addEventListener("submit", handleSubmit)
+}
+
 function paintGreeting(text){
     console.log(text);
     form.classList.remove(SHOWING_CN);
@@ -13,17 +30,13 @@ function paintGreeting(text){
 }
 
 function loadName(){
-    console.log("HI");
     const currentUser = localStorage.getItem(USER_LS);
-    console.log(currentUser);
     if (currentUser === null){
         // user is not
-        console.log("No");
-        console.log(currentUser);
+        askForName();
+
     } else {0
         // there is user
-        console.log("YES");
-        console.log(currentUser);
         paintGreeting(currentUser);
 
     }
